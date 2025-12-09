9a4837f9deac096a3b07e9164e67f62058433683
Evgenii <evgenii@MacBook-Air-Evgenii.local>
2025-12-09 20:44:20 +0300

    push current websocket version

--- chatrooms/models.py
+++ chatrooms/models.py
@@ -4,6 +4,7 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.urls import reverse
 from hospital.models import Patient
+from django.conf import settings
 
 class Room(models.Model):
     name = models.CharField(max_length=64, unique=True)
